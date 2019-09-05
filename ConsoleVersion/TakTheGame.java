d29253ba20a1f58b5eb2652c611252e311fee592
Jacob Dyer <33190456+JacobLD@users.noreply.github.com>
2019-09-05 14:16:53 -0500

    Changed name of main program to TakTheGame

--- ConsoleVersion/TakTheGame.java
+++ ConsoleVersion/TakTheGame.java
@@ -1,8 +1,8 @@
 package ConsoleVersion;
 
-class main {
+class TakTheGame {
     public static void main(String[] args){
-        Board board = new Board();
+        Board board = new Board(5);
         System.out.println(board);
     }
 }
