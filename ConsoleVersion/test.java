ff2cd0ba6b19b897e0583d2681a4b726230e4132
Jacob Dyer <33190456+JacobLD@users.noreply.github.com>
2019-09-12 22:37:25 -0500

    Works
    These two goofballs can play all day
    NEED - limited pieces

--- ConsoleVersion/test.java
+++ ConsoleVersion/test.java
@@ -2,13 +2,15 @@ class test{
     public static void main(String[] args){
 
         final int SIZE = 6;
-        Board board = new board(SIZE);
+        Board board = new Board(SIZE);
         BOGOAI player1 = new BOGOAI(board, true);
         BOGOAI player2 = new BOGOAI(board, false);
 
         while(!board.determineWinner()){
             player1.makeMove();
             player2.makeMove();
+            System.out.println(board);
         }
+        System.out.println(board.getWinner());
     }
 }
