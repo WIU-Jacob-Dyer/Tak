c3bd6c5dfcd06edfa57bdcac5a96b536c382c109
Jacob Dyer <33190456+JacobLD@users.noreply.github.com>
2019-12-04 12:00:10 -0600

    Update TakPiece.java
    Added Javadocs

--- ConsoleVersion/TakPiece.java
+++ ConsoleVersion/TakPiece.java
@@ -5,6 +5,12 @@ class TakPiece{
     private boolean wall;
     private boolean road;
 
+    /**
+     * Creates new TakPiece.
+     * @param white Is this piece white?
+     * @param wall Is this a wall?
+     * @param capstone Is this a capstone?
+     */
     public TakPiece(boolean white, boolean wall, boolean capstone){
         this.white = white;
 
@@ -19,13 +25,31 @@ class TakPiece{
         }
     }
 
+    /**
+     * Demolish wall into road.
+     */
     public void crush(){
         this.wall = false;
     }
 
+    /**
+     * @return isWhite
+     */
     public boolean isWhite()  {return white;}
+    /**
+     * @return isBlack
+     */
     public boolean isBlack()  {return !white;}
+    /**
+     * @return isWall
+     */
     public boolean isWall()   {return wall;}
+    /**
+     * @return isRoad
+     */
     public boolean isRoad()   {return road;}
+    /**
+     * @return isCapstone
+     */
     public boolean isCapstone()   {return capstone;}
 }
