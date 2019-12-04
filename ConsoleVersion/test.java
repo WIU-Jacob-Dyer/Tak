00f9b3e48fd057874361b28d4b181c707d3579bb
Jacob Dyer <33190456+JacobLD@users.noreply.github.com>
2019-12-04 11:37:01 -0600

    Update test.java

--- ConsoleVersion/test.java
+++ ConsoleVersion/test.java
@@ -1,7 +1,7 @@
 class test{
     public static void main(String[] args){
 
-        final int SIZE = 5;
+        final int SIZE = 8;
         Board board = new Board(SIZE);
         BOGOAI player1 = new BOGOAI(board, true);
         BOGOAI player2 = new BOGOAI(board, false);
@@ -9,9 +9,8 @@ class test{
         while(!board.determineWinner()){
             player1.makeMove();
             player2.makeMove();
-            System.out.println(board);
         }
-
+        System.out.println(board);
         System.out.println(board.getWinner());
     }
 }
